24850515e0848d61a51a74b40b89a79dd112cb1b
reio <reiokai@gmail.com>
2020-08-23 15:42:17 +0800

    Validation Form complete

--- w5-formValidation/all.js
+++ w5-formValidation/all.js
@@ -1,15 +1,16 @@
-// 全域註冊 input 驗證元件
-Vue.component('ValidationProvider', VeeValidate.ValidationProvider);
-// 全域註冊表單驗證元件
-Vue.component('ValidationObserver', VeeValidate.ValidationObserver);
-// 全域註冊 VueLoading 元件
-Vue.component('loading', VueLoading);
 // 匯入中文語系檔案
-import zh_TW from "./js/zh_TW.js";
+import zh_TW from "./zh_TW.js";
+
 // 載入語系設定檔案到 VeeValidate，使用 import zh_TW 多國語系
 VeeValidate.localize('tw', zh_TW);
 
-//自定義設定檔案，依據不同驗證狀態，設定對應的 className，bootstrap 驗證綠色/紅色樣式
+// 全域註冊 input 驗證元件
+Vue.component('ValidationProvider', VeeValidate.ValidationProvider);
+
+// 全域註冊表單驗證元件
+Vue.component('ValidationObserver', VeeValidate.ValidationObserver);
+
+// 自定義設定檔案，依據不同驗證狀態，設定對應的 className，bootstrap 驗證綠色/紅色樣式
 VeeValidate.configure({
   classes: {
     valid: 'is-valid',
@@ -28,6 +29,10 @@ new Vue({
       payment: '',
       message: '',
     }
-
+  },
+  methods: {
+    submitForm() {
+      console.log("已送出表單");
+    }
   },
 });
